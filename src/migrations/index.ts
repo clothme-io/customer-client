f7b9ba47116d0789f8b32b61efe9d3f622fd601d
clothme <57736523+clothme-io@users.noreply.github.com>
2026-07-04 10:28:27 -0700

    chore: fix post UI

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20260626_183444 from './20260626_183444';
 import * as migration_20260702_000001_waitlist_entries from './20260702_000001_waitlist_entries';
 import * as migration_20260702_000002_drop_legacy_tables from './20260702_000002_drop_legacy_tables';
 import * as migration_20260703_000001_webhook_ingest_repair from './20260703_000001_webhook_ingest_repair';
+import * as migration_20260704_000001_reset_cms_posts_preferences from './20260704_000001_reset_cms_posts_preferences';
 
 export const migrations = [
   {
@@ -23,5 +24,10 @@ export const migrations = [
     up: migration_20260703_000001_webhook_ingest_repair.up,
     down: migration_20260703_000001_webhook_ingest_repair.down,
     name: '20260703_000001_webhook_ingest_repair'
+  },
+  {
+    up: migration_20260704_000001_reset_cms_posts_preferences.up,
+    down: migration_20260704_000001_reset_cms_posts_preferences.down,
+    name: '20260704_000001_reset_cms_posts_preferences'
   },
 ];
