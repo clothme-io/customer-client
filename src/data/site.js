751b0cb2af055713f573a9c8d7c3fcee031046ec
clothme <57736523+clothme-io@users.noreply.github.com>
2026-07-22 18:38:22 -0700

    chore: updated env

--- src/data/site.js
+++ src/data/site.js
@@ -6,7 +6,7 @@ export const siteConfig = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || process.env.VITE_SITE_URL || "https://clothme.app",
   twitterHandle: "",
   analytics: {
-    gaId: process.env.NEXT_PUBLIC_GA_ID || process.env.VITE_GA_ID || "",
+    gaId: process.env.NEXT_PUBLIC_GA_ID || process.env.VITE_GA_ID || "G-8H7MC8EYN1",
     gtmId: process.env.NEXT_PUBLIC_GTM_ID || process.env.VITE_GTM_ID || "",
     plausibleDomain: process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN || process.env.VITE_PLAUSIBLE_DOMAIN || ""
   }
